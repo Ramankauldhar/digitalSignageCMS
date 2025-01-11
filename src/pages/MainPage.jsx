4b52751f6ceb31f17834831f343cc44db0705c08
Ramankauldhar <rmnkaul979697@gmail.com>
2025-01-11 18:30:44 -0500

    V3:Managed state of visibility of tool options

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,6 +4,24 @@ import '../styles/mainPageStyles.css';
 const MainPage = () => {
     const [inputValue, setInputValue] = useState('');
 
+    //state to track visibiity of options for each widget group
+    const [visibleWidgets, setVisibleWidgets] = useState({
+      wid1: true,
+      wid2: true,
+      wid3: true,
+      wid4: true,
+      wid5: true,
+      wid6: true,
+      wid7: true,
+    });
+
+    const toggleVisibility = (widgetKey) =>{
+      setVisibleWidgets((prevState) => ({
+          ...prevState,
+          [widgetKey]: !prevState[widgetKey],
+      }));
+    };
+
     return(
         <>
           <div>
@@ -30,84 +48,139 @@ const MainPage = () => {
                     </div>
                     <div className="widgetsContainer">
                         <div className="widgetGroup">
-                            <div className="widgets wid1">
-                                 <i className="fas fa-angle-down"></i>
+                            <div className="widgets wid1" onClick={() => toggleVisibility('wid1')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid1 ? 'down' : 'right'}`}></i>
                                  <span>MARKER</span>
                             </div>
-                            <div className="options opt1">
-                                 <i className="fas fa-map-marker-alt"></i>
-                                 <button>Marker</button>
-                            </div>
+                            {visibleWidgets.wid1 && (
+                              <div className="options opt1">
+                                    <i className="fas fa-map-marker-alt"></i>
+                                    <button>Marker</button>
+                              </div>
+                            )}
                             <hr></hr>
                         </div>
                         <div className="widgetGroup">
-                            <div className="widgets wid2">
-                                 <i className="fas fa-angle-down"></i>
+                            <div className="widgets wid2" onClick={() => toggleVisibility('wid2')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid2 ? 'down' : 'right'}`}></i>
                                  <span>TEXT</span>
                             </div>
-                            <div className="options opt2">
-                                 <i className="fas fa-font"></i>
-                                 <button>Text</button>
-                            </div>
+                            {visibleWidgets.wid2 && (
+                                <div className="options opt2">
+                                    <i className="fas fa-font"></i>
+                                    <button>Text</button>
+                                </div>
+                            )}
                         </div>
                         <hr></hr>
                         <div className="widgetGroup">
-                            <div className="widgets wid3">
-                                 <i className="fas fa-angle-down"></i>
+                            <div className="widgets wid3" onClick={() => toggleVisibility('wid3')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid3 ? 'down' : 'right'}`}></i>
                                  <span>IMAGE</span>
                             </div>
-                            <div className="options opt3">
-                                 <i className="fas fa-image"></i>
-                                 <button>Image</button>
-                            </div>
-                            <div className="options opt3">
-                                 <i className="fas fa-image"></i>
-                                 <button>GIF</button>
-                            </div>
+                            {visibleWidgets.wid3 && (
+                                <>
+                                    <div className="options opt3">
+                                        <i className="fas fa-image"></i>
+                                        <button>Image</button>
+                                    </div>
+                                    <div className="options opt3">
+                                        <i className="fas fa-image"></i>
+                                        <button>GIF</button>
+                                    </div>
+                                </>
+                            )}
                         </div>
                         <hr></hr>
                         <div className="widgetGroup">
-                            <div className="widgets wid4">
-                                 <i className="fas fa-angle-down"></i>
+                            <div className="widgets wid4" onClick={() => toggleVisibility('wid4')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid4 ? 'down' : 'right'}`}></i>
                                  <span>SHAPE</span>
                             </div>
-                            <div className="options opt4">
-                                 <i className="fas fa-duotone fa-regular fa-caret-up triangle"></i>
-                                 <button>Triangle</button>
-                            </div>
-                            <div className="options opt4">
-                                 <i className="fas fa-stop"></i>
-                                 <button>Rectangle</button>
-                            </div>
-                            <div className="options opt4">
-                                 <i className="fas fa-solid fa-circle"></i>
-                                 <button>Circle</button>
-                            </div>
-                            <div className="options opt4">
-                                 <i className="fas fa-cube"></i>
-                                 <button>Cube</button>
-                            </div>
+                            {visibleWidgets.wid4 && (
+                                <>
+                                    <div className="options opt4">
+                                        <i className="fas fa-caret-up triangle"></i>
+                                        <button>Triangle</button>
+                                    </div>
+                                    <div className="options opt4">
+                                        <i className="fas fa-stop"></i>
+                                        <button>Rectangle</button>
+                                    </div>
+                                    <div className="options opt4">
+                                        <i className="fas fa-circle"></i>
+                                        <button>Circle</button>
+                                    </div>
+                                    <div className="options opt4">
+                                        <i className="fas fa-cube"></i>
+                                        <button>Cube</button>
+                                    </div>
+                                </>
+                              )}
                         </div>
                         <hr></hr>
                         <div className="widgetGroup">
-                            <div className="widgets wid5">
-                                 <i className="fas fa-angle-down"></i>
+                            <div className="widgets wid5" onClick={() => toggleVisibility('wid5')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid5 ? 'down' : 'right'}`}></i>
                                  <span>DRAWING</span>
                             </div>
-                            <div className="options opt5">
-                                 <i className="fas fa-draw-polygon"></i>
-                                 <button>Polygon</button>
-                            </div>
-                            <div className="options opt5">
-                                 <i className="fas fa-solid fa-minus"></i>
-                                 <button>Line</button>
-                            </div>
-                            <div className="options opt5">
-                                 <i className="fas fa-light fa-arrow-right"></i>
-                                 <button>Arrow</button>
-                            </div>
+                            {visibleWidgets.wid5 && (
+                                <>
+                                    <div className="options opt5">
+                                        <i className="fas fa-draw-polygon"></i>
+                                        <button>Polygon</button>
+                                    </div>
+                                    <div className="options opt5">
+                                        <i className="fas fa-minus"></i>
+                                        <button>Line</button>
+                                    </div>
+                                    <div className="options opt5">
+                                        <i className="fas fa-arrow-right"></i>
+                                        <button>Arrow</button>
+                                    </div>
+                                </>
+                            )}
                         </div>
                         <hr></hr>
+                        <div className="widgetGroup">
+                            <div className="widgets wid6" onClick={() => toggleVisibility('wid6')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid6 ? 'down' : 'right'}`}></i>
+                                 <span>ELEMENT</span>
+                            </div>
+                            {visibleWidgets.wid6 && (
+                                <>
+                                    <div className="options opt6">
+                                        <i className="fas fa-chart-line"></i>
+                                        <button>Chart</button>
+                                    </div>
+                                    <div className="options opt6">
+                                        <i className="fab fa-html5"></i>
+                                        <button>Element</button>
+                                    </div>
+                                    <div className="options opt6">
+                                        <i className="fas fa-window-maximize"></i>
+                                        <button>Iframe</button>
+                                    </div>
+                                    <div className="options opt6">
+                                        <i className="fas fa-video"></i>
+                                        <button>Video</button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        <hr></hr>
+                        <div className="widgetGroup">
+                            <div className="widgets wid7" onClick={() => toggleVisibility('wid7')}>
+                                 <i className={`fas fa-angle-${visibleWidgets.wid7 ? 'down' : 'right'}`}></i>
+                                 <span>SVG</span>
+                            </div>
+                            {visibleWidgets.wid7 && (
+                                <div className="options opt7">
+                                    <i className="fas fa-thin fa-sitemap"></i>
+                                    <button>Default</button>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
 
